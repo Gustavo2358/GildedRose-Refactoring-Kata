3a0317e8903ba91b0ead51aaa3616707e8aa1c19
Gustavo Oliveira <gustavo28064212@gmail.com>
2024-04-07 14:45:30 -0300

    substitute conditional with pattern matching

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,28 +12,12 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (isAgedBrie(item)) {
-                new AgedBrie(item).updateAgedBrie();
-            } else if (isBackstagePasses(item)) {
-                new BackstagePasses(item).updatedBackstagePasses();
-            } else if (isSulfuras(item)) {
-                new Sulfuras(item).updateSulfuras();
-            }
-            else {
-                new DefaultItem(item).updateDefaultItem();
+            switch (item.name) {
+                case AGED_BRIE -> new AgedBrie(item).updateAgedBrie();
+                case BACKSTAGE_PASSES -> new BackstagePasses(item).updatedBackstagePasses();
+                case SULFURAS -> new Sulfuras(item).updateSulfuras();
+                default -> new DefaultItem(item).updateDefaultItem();
             }
         }
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    private boolean isBackstagePasses(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
