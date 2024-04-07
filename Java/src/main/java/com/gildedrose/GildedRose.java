d172f71d5a61bdad4e29a3f6d91996ccbfa34247
Gustavo Oliveira <gustavo28064212@gmail.com>
2024-04-06 23:33:51 -0300

    separate sellin update from quality update

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -26,27 +26,28 @@ class GildedRose {
                 }
             }
 
-            if (!isSulfuras(item)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!isAgedBrie(item)) {
-                    if (!isBackstagePasses(item)) {
+            if (item.sellIn < 1) {
+                if (isAgedBrie(item)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (isBackstagePasses(item)) {
+                        item.quality = 0;
+                    } else {
                         if (item.quality > 0) {
                             if (!isSulfuras(item)) {
                                 item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
                     }
                 }
             }
+
+            if (!isSulfuras(item)) {
+                item.sellIn = item.sellIn - 1;
+            }
+
         }
     }
 
